ea5ceaa038cebe8ca4289b513a5670eed534be5d
Caio <caiocpe@gmail.com>
2024-11-12 04:18:28 -0300

    reset repo

--- src/domain/repositories/clients-repository.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from "../entities/client";
-
-export abstract class ClientsRepository{
-    abstract create(client: Client): Promise<void>
-}
--- src/domain/repositories/messages-repository.ts
+++
@@ -1,5 +0,0 @@
-import { Message } from "../entities/message";
-
-export abstract class MessagesRepository {
-    abstract create(message: Message): Promise<void>
-}
