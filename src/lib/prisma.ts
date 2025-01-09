37d34e8543aeeaa2e1530a4f73460912d2f15abf
Caio <caiocpe@gmail.com>
2025-01-09 17:58:58 -0300

    chore: avoiding multiple connections from Prisma to DB

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,22 @@
 import { env } from '@/env'
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
-  log: env.NODE_ENV === 'dev' ? ['query'] : [],
-})
+declare global {
+  var cachedPrism: PrismaClient
+}
+
+let prisma: PrismaClient
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({
+    log: env.NODE_ENV === 'dev' ? ['query'] : [],
+  })
+} else {
+  if (!global.cachedPrism) {
+    global.cachedPrism = new PrismaClient({
+      log: env.NODE_ENV === 'dev' ? ['query'] : [],
+    })
+  }
+  prisma = global.cachedPrism
+}
+
+export { prisma }
