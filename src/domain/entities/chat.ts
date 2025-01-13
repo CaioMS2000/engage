599745396decb9f0eae462587ef01d8520469ea4
Caio <caiocpe@gmail.com>
2025-01-13 17:54:32 -0300

    chore: slug value-object

--- src/domain/entities/chat.ts
+++ src/domain/entities/chat.ts
@@ -1,4 +1,5 @@
 import { randomUUID } from 'node:crypto'
+import { Slug } from './value-objecs/slug'
 
 // biome-ignore lint/style/useEnumInitializers:
 enum ChatStatus {
@@ -9,6 +10,7 @@ enum ChatStatus {
 type ChatProps = {
 	companyId: string
 	clientId: string
+	slug: Slug
 	status: ChatStatus
 	createdAt: Date
 	updatedAt: Date
@@ -17,17 +19,19 @@ type ChatProps = {
 export class Chat {
 	public id: string
 	public companyId: string
+	public slug: Slug
 	public clientId: string
 	public status: ChatStatus
 	public createdAt: Date
 	public updatedAt: Date
 
 	constructor(props: ChatProps, id?: string) {
-		const { companyId, clientId, status, createdAt, updatedAt } = props
+		const { companyId, clientId, status, createdAt, updatedAt, slug } = props
 		this.id = id ?? randomUUID().toString()
 		this.companyId = companyId
 		this.clientId = clientId
 		this.status = status
+		this.slug = slug
 		this.createdAt = createdAt ?? new Date()
 		this.updatedAt = updatedAt ?? new Date()
 	}
